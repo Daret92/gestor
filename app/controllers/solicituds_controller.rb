096f106faf49ff8f06f8088de898281bb1d95a05
Jorge Daret <daret.caravantes@gmail.com>
2019-05-21 10:25:03 -0500

    Fixing

--- app/controllers/solicituds_controller.rb
+++ app/controllers/solicituds_controller.rb
@@ -252,8 +252,11 @@ class SolicitudsController < ApplicationController
       contenido = contenido +"\n Solicitud Autorizada"
     end
     if jefe
-      sendNotificacion("Genero Solicitud","Genero una solicitud "+validato.user.nombre+", con folio: "+validato.id.to_s+", Para el proyecto "+validato.proyecto.titulo+"\nContenido de la solicitud:\n"+ contenido,jefe.token_msj)
-      #Telegram.bot.send_message(chat_id: jefe.token_msj, text: "Genero una solicitud "+validato.user.nombre+", Para el proyecto "+validato.proyecto.titulo+"\nContenido de la solicitud:\n"+ contenido+"\n" +"<a href='http://gestor.tuperfil.com.mx/solicituds/"+validato.id.to_s+"'>Revisar Solicitud</a>",parse_mode: "HTML")
+      begin
+        Telegram.bot.send_message(chat_id: jefe.token_msj, text: "Genero una solicitud "+validato.user.nombre+", Para el proyecto "+validato.proyecto.titulo+"\nContenido de la solicitud:\n"+ contenido+"\n" +"<a href='http://gestor.tuperfil.com.mx/solicituds/"+validato.id.to_s+"'>Revisar Solicitud</a>",parse_mode: "HTML")
+      rescue 
+        sendNotificacion("Genero Solicitud","Genero una solicitud "+validato.user.nombre+", con folio: "+validato.id.to_s+", Para el proyecto "+validato.proyecto.titulo+"\nContenido de la solicitud:\n"+ contenido,jefe.token_msj)
+      end
     else
       Telegram.bot.send_message(chat_id: 340614248, text: "Genero una solicitud "+validato.user.nombre+", Para el proyecto "+validato.proyecto.titulo+"\nContenido de la solicitud:\n"+ contenido+"\n" +"No se envio a un supervisor favor de realizar el aviso a quien corresponde",parse_mode: "HTML")
     end
