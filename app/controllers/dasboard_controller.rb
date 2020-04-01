e8cf7fc92b35aca7fc3819007419a05bce6ed4e4
Escritorio <escritorio@daret>
2020-04-01 13:37:44 -0600

    Fixing

--- app/controllers/dasboard_controller.rb
+++ app/controllers/dasboard_controller.rb
@@ -2,11 +2,12 @@ class DasboardController < ApplicationController
   def index
     @homework = HomeWork.new
   	@registry = Registry.new
-  	@lastRegistry = Registry.where(user:current_user).limit(5).order('id DESC')
+  	
   	if current_user.super_user or current_user.rol.nombre == "Gerente"
       @solicitudes = Solicitud.where(estado:"1")
       @all_solicitudes = Solicitud.all()
       @tareas = HomeWork.where(finalizado:false)
+      @lastRegistry = Registry.all().limit(5).order('created_at ASC')
     else  
       rol =  current_user.rol.nombre
       if rol == "JefeDepartamento"
@@ -20,9 +21,15 @@ class DasboardController < ApplicationController
         @solicitudes = Solicitud.where(id:ids,estado:"1")
 
         @tareas = HomeWork.where(administrador:current_user,finalizado:false)
+        usuarios = User.where(users_id:current_user.id)
+        @lastRegistry = Registry.where(user:usuarios.ids).limit(5).order('created_at ASC')
+      elsif @permissions_user.include?("Bitacora")        
+        usuarios = User.where(users_id:current_user.id)
+        @lastRegistry = Registry.where(user:usuarios.ids).limit(5).order('created_at ASC')
       else
         @solicitudes = Solicitud.where(user:current_user,estado:"1")
         @tareas = HomeWork.where(usuario:current_user,finalizado:false)
+        @lastRegistry = Registry.where(user:current_user).limit(5).order('id DESC')
       end
     end
   end
